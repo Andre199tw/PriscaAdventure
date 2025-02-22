f489ae7c8e4d12a9d5915e0c6fb143b2513ded0b
Andre199 <130306826+Andre199tw@users.noreply.github.com>
2025-02-22 22:48:41 +0100

    Update CardManager.gd

--- priscaadventure/CardManager.gd
+++ priscaadventure/CardManager.gd
@@ -3,6 +3,8 @@ extends Node2D
 const COLLISION_MASK_CARD=1
 var carta_sposta
 var dim_screen
+var is_hover_carta
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -21,10 +23,18 @@ func _input(event):
 		if event.pressed:
 			var carta=spostamento()
 			if carta:
-				carta_sposta=carta
+				inizio_spostamento(carta)
 		else:
-			carta_sposta=null
+			fine_spostamento()
 
+func inizio_spostamento(carta):
+	carta_sposta=carta
+	carta.scale=Vector2(1,1)
+	
+
+func fine_spostamento():
+	carta_sposta.scale=Vector2(1.2,1.2)
+	carta_sposta=null
 
 
 func spostamento():
@@ -35,20 +45,39 @@ func spostamento():
 	parameters.collision_mask=COLLISION_MASK_CARD
 	var result=space_state.intersect_point(parameters)
 	if result.size()>0:
-		return result[0].collider.get_parent()
+		#controllo su z-index di hover
+		return carta_maggiore_z_index(result)
 	else:
 		return null
 
+
+#permette di selezionare e rispostare la carta con z-index più alto, quindi la carta già hoverata
+func carta_maggiore_z_index(carte):
+	var carta_maggiore_z=carte[0].collider.get_parent()
+	var maggiore_z_index= carta_maggiore_z.z_index
+	for i in range(1,carte.size()):
+		var carta_corrente=carte[i].collider.get_parent()
+		if carta_corrente.z_index>maggiore_z_index:
+			maggiore_z_index=carta_corrente.z_index
+			carta_maggiore_z=carta_corrente
+	return carta_maggiore_z
 
 func connetti_carta(carta):
 	carta.connect("hovered", carta_hover)
 	carta.connect("hovered_off", carta_non_hover)
 	
 func carta_hover(carta):
-	effetto_hover(carta,true)
+	if !is_hover_carta:
+		is_hover_carta=true
+		effetto_hover(carta,true)
 	
 func carta_non_hover(carta):
 	effetto_hover(carta,false)
+	var nuova_carta_hover=spostamento()
+	if nuova_carta_hover:
+		effetto_hover(nuova_carta_hover, true)
+	else:
+		is_hover_carta=false
 
 
 
